f8758f9fb6f749dff92f089049b697df10e17698
NsRF <nasserfarhatt@gmail.com>
2024-03-28 21:26:15 -0300

    Subindo correções a respeito do chatbot em que ele não enviava as opções de Menu, após a primeira tentativa, além de sempre retornar opção inválida para qualquer mensagem digitada.

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,17 +1,37 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth} = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const { abrirChamado } = require('../api/chamados');
 
-const client = new Client();
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
 
 let isFirstMessage = true;
+let afterSendOptionMenu = false;
 
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
     console.log('QR Code gerado. Escaneie-o para se autenticar:', qr);
 });
 
-client.on('ready', () => {
+// Não tem porque ter o método waitForDescription e waitForLogin, sendo que os dois fazem a mesma função, deixe-os com um nome genérico e sempre que precisar, o chame.
+// Função para aguardar a próxima mensagem do usuário
+function waitingForMessage(sender) {
+    const waitFor = new Promise(resolve => {
+        client.on('message', async (message) => {
+            if (message.from === sender) {
+                resolve(message.body.trim());
+            }
+        });
+    });
+    return waitFor;
+}
+//Mano deixei segregado por poder ser um método utilizado várias vezes, com isso, basta você chamar o método e enviar o sender como parâmetro.
+const sendDefaultMessage = async (sender) => {
+    await client.sendMessage(sender, 'Olá! Escolha uma das opções a seguir:\n`1 - Abrir chamado`');
+}
+
+client.on('ready',  async () => {
     console.log('WhatsApp client está pronto');
 
     client.on('message', async (message) => {
@@ -19,47 +39,29 @@ client.on('ready', () => {
             const sender = message.from;
             const text = message.body;
 
-            // Verifica se é a primeira mensagem recebida ou se houve um erro na abertura do chamado
+
             if (isFirstMessage) {
-                // Responde apenas à primeira mensagem de saudação
                 isFirstMessage = false;
-                await client.sendMessage(sender, 'Olá! Escolha uma das opções a seguir:\n1 - Abrir chamado');
+                await sendDefaultMessage(sender);
                 return;
+            }
+
+            //Exemplo de uso 2x do mesmo método, criado acima.
+            if(text.toLowerCase().includes('menu')){
+                await sendDefaultMessage(sender);
             }
 
             // Verifica se a mensagem contém o texto "1" (opção para abrir chamado)
             if (text && text.trim() === '1') {
-                // Envia uma mensagem solicitando o login do usuário
+                afterSendOptionMenu = true;
                 await client.sendMessage(sender, 'Por favor, informe o seu login:');
 
-                // Função para aguardar a próxima mensagem do usuário
-                const waitForLogin = new Promise(resolve => {
-                    client.on('message', async (message) => {
-                        if (message.from === sender) {
-                            resolve(message.body.trim());
-                        }
-                    });
-                });
+                const login = await waitingForMessage(sender);
 
-                // Aguarda a resposta do usuário
-                const login = await waitForLogin;
-
-                // Envia uma mensagem solicitando a descrição do chamado
                 await client.sendMessage(sender, 'Por favor, descreva o chamado:');
 
-                // Função para aguardar a próxima mensagem do usuário
-                const waitForDescricao = new Promise(resolve => {
-                    client.on('message', async (message) => {
-                        if (message.from === sender) {
-                            resolve(message.body.trim());
-                        }
-                    });
-                });
+                const descricao = await waitingForMessage(sender);
 
-                // Aguarda a resposta do usuário
-                const descricao = await waitForDescricao;
-
-                // Informações fixas do chamado
                 const dadosChamado = {
                     CatalogoServicosid: 2089, // Definindo o ID do catálogo de serviços como 2089
                     Urgencia: 3, // Definindo a urgência como 3 (Alta) diretamente
@@ -70,6 +72,7 @@ client.on('ready', () => {
 
                 try {
                     const resposta = await abrirChamado(dadosChamado);
+                    await client.sendMessage(sender, 'Aguarde... Abrindo o chamado.')
                     console.log('Chamado aberto com sucesso:', resposta);
                     // Envia uma mensagem de confirmação para o remetente
                     await client.sendMessage(sender, 'Chamado aberto com sucesso! Número do chamado: ' + resposta);
@@ -80,6 +83,8 @@ client.on('ready', () => {
                     await client.sendMessage(sender, 'Erro ao abrir chamado. Por favor, tente novamente.');
                     isFirstMessage = true; // Reinicia o loop
                 }
+            } else if(!text.toLowerCase().includes("menu") && !afterSendOptionMenu){
+                await client.sendMessage(sender, "Opção inválida, por favor selecione uma opção válida. Escreva menu, para obter as opções.")
             }
             
         }
